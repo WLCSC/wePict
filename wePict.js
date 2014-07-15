8734a0d2433ca53734d52b08f12b2b38937a1d56
Sammidysam <sammidysam@gmail.com>
2014-07-15 12:34:05 -0400

    Add all necessary collections

--- wePict.js
+++ wePict.js
@@ -1,3 +1,9 @@
+// Questions are embedded in banks.
+Banks = new Meteor.Collection("banks");
+Groups = new Meteor.Collection("groups");
+Memberships = new Meteor.Collection("memberships");
+Posts = new Meteor.Collection("posts");
+// Assignments are embedded in rooms.
 Rooms = new Meteor.Collection("rooms");
 
 if (Meteor.isClient) {
