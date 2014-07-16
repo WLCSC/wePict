8cc742e877b6ac914db388867b481bc2d8705e2a
Sammidysam <sammidysam@gmail.com>
2014-07-16 11:15:51 -0400

    Remove template

--- wePict.js
+++ wePict.js
@@ -7,17 +7,7 @@ Posts = new Meteor.Collection("posts");
 Rooms = new Meteor.Collection("rooms");
 
 if (Meteor.isClient) {
-	Template.hello.greeting = function () {
-		return "Welcome to wePict.";
-	};
-
-	Template.hello.events({
-		'click input': function () {
-			// template data, if any, is available in 'this'
-			if (typeof console !== 'undefined')
-				console.log("You pressed the button");
-		}
-	});
+	// Client code, such as template stuff
 }
 
 if (Meteor.isServer) {
