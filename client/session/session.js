fa37e47fa1e091840bafe0bfa2a61a893176c257
Sammidysam <sammidysam@gmail.com>
2014-07-23 11:22:26 -0400

    Add session code

---
+++ client/session/session.js
@@ -0,0 +1,21 @@
+Template.loginForm.events({
+	"submit #login-form": function(event, template) {
+		event.preventDefault();
+
+		Meter.loginWithGoogle(function (error) {
+			if (error)
+				alert(error);
+		});
+	}
+});
+
+Template.logoutForm.events({
+	"submit #logout-form": function(event, template) {
+		event.preventDefault();
+
+		Meteor.logout(function(error) {
+			if (error)
+				alert(error);
+		});
+	}
+});
