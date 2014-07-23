803c637d6a71192892c1c4d7be8bf0bb84dad6d4
Sammidysam <sammidysam@gmail.com>
2014-07-23 11:50:59 -0400

    Fix bug with log in button
    Meteor has an "o"

--- client/session/session.js
+++ client/session/session.js
@@ -1,8 +1,8 @@
 Template.loginForm.events({
 	"submit #login-form": function(event, template) {
 		event.preventDefault();
-
-		Meter.loginWithGoogle(function (error) {
+		
+		Meteor.loginWithGoogle(function (error) {
 			if (error)
 				alert(error);
 		});
