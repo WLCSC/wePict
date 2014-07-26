5bbdfaf092a26fb04b0ba02ef46c017dcf17c025
Sammidysam <sammidysam@gmail.com>
2014-07-25 21:57:23 -0400

    Authenticate user correctly

--- server/methods.js
+++ server/methods.js
@@ -1,14 +1,27 @@
 Meteor.methods({
+	getAccessToken: function() {
+		try {
+			return Meteor.user().services.google.accessToken;
+		} catch (e) {
+			return null;
+		}
+	},
+	
 	isAdmin: function() {
 		this.unblock();
 
 		try {
 			var result_data = HTTP.get("https://www.googleapis.com/admin/directory/v1/groups", {
+				headers: {
+					"Authorization": "Bearer " + Meteor.call('getAccessToken')
+				},
 				params: {
 					domain: "wl.k12.in.us",
 					customer: "my_customer"
 				}
 			}).data;
+
+			console.log(result_data);
 
 			for (var i = 0; i < result_data.groups.length; i++)
 				if (result_data.groups[i].email == "TechnologyStaff@wl.k12.in.us")
@@ -16,6 +29,7 @@ Meteor.methods({
 
 			return false;
 		} catch (e) {
+			console.log(e);
 			return false;
 		}
 	}
