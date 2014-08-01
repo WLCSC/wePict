21ce4af3989ad2592be89491ee7b9992e4685a07
Sammidysam <sammidysam@gmail.com>
2014-08-01 13:53:25 -0400

    Subscribe when needed

--- collections/room.js
+++ collections/room.js
@@ -1,6 +1,12 @@
 // Assignments are embedded in rooms.
 Rooms = new Meteor.Collection("rooms");
 
-Meteor.publish("rooms", function() {
-	return Rooms.find({});
-});
+if (Meteor.isClient) {
+	Meteor.subscribe("rooms");
+}
+
+if (Meteor.isServer) {
+	Meteor.publish("rooms", function() {
+		return Rooms.find({});
+	});
+}
